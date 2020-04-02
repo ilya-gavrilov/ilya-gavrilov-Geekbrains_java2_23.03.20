25bb92f07414b2d874db590883904ba65d0dcc40
Илья <ilyagavrilov@mail.ru>
2020-04-02 15:21:41 +0300

    Homework 2.

---
+++ src/lesson2/homework/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package lesson2.homework;
+
+public class MyArrayDataException extends RuntimeException {
+
+    public MyArrayDataException(String cellValue, int rowIndex, int colIndex) {
+        super(String.format("Invalid value '%s' in array cell[%d][%d], required integer",
+                cellValue, rowIndex, colIndex));
+    }
+}
+
