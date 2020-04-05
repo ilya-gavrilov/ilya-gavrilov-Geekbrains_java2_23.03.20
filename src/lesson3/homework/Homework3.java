c7f418b5d24cc2c9fec17d2c608da2a80126d41f
Илья <ilyagavrilov@mail.ru>
2020-04-05 12:42:12 +0300

    test git 5

--- src/lesson3/homework/Homework3.java
+++ src/lesson3/homework/Homework3.java
@@ -3,6 +3,7 @@ package src.lesson3.homework;
 public class Homework3 {
     public static void main(String[] args) {
         System.out.println("Test git branches.");
+        //Играюсь с гитом. Тест.
 
     }
 }
