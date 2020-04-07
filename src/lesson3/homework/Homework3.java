ccf655ee05899d0d5b3eebe893b25c53faf695a8
Илья <ilyagavrilov@mail.ru>
2020-04-07 16:51:18 +0300

    Resolve conflict.

--- src/lesson3/homework/Homework3.java
+++ src/lesson3/homework/Homework3.java
@@ -3,7 +3,5 @@ package src.lesson3.homework;
 public class Homework3 {
     public static void main(String[] args) {
         System.out.println("Test git branches.");
-        //Играюсь с гитом. Тест.
-
     }
 }
