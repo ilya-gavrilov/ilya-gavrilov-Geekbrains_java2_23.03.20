eab7a38b3fffc402a6c726b169f0bc0971b58740
Илья <ilyagavrilov@mail.ru>
2020-04-20 15:26:37 +0300

    Homework 7. Поздно скачал исходники, личные сообщения уже были реализованы. Поэтому ничего не менял по этой теме, только разбирался в коде, изучал. Продолжаю ломать голову над отключением неавторизовнных пользователей.

--- src/lesson7/homework/Homework7.java
+++
@@ -1,4 +0,0 @@
-package src.lesson7.homework;
-
-public class Homework7 {
-}
---
+++ src/lesson7/homework/server/ServerApp.java
@@ -0,0 +1,7 @@
+package src.lesson7.homework.server;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
